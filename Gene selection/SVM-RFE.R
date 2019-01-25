9fc540bbcbb25ffd0bd309289274dd26e2981b9e
yinlisssss <38618580+yinlisssss@users.noreply.github.com>
2019-01-25 11:24:04 +0800

    Create SVM-RFE.R

---
+++ Gene selection/SVM-RFE.R
@@ -0,0 +1,5 @@
+#######This algorithm could take huge time, thus parallel computing is required.
+#######May not work on Windows environment (see R package 'parallel' for more information).
+
+library(e1071)
+library(parallel)
